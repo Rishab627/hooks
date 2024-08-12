f1bd41ab7e370d19786133579a70a89f24a8c45a
Rishab627 <merishabjoshi@gmail.com>
2024-08-12 15:26:08 +0545

    ch

--- src/features/auth/Register.jsx
+++ src/features/auth/Register.jsx
@@ -24,7 +24,7 @@ import { useNavigate } from "react-router";
             </Typography>
             <Input
               size="lg"
-              placeholder="name@mail.com"
+              placeholder="Your Name"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
                 className: "before:content-none after:content-none",
